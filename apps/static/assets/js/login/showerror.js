b558c73ec44fde13ce8aa640d7afb3bd82730474
samdanikouser <samdani.shareef6@gmail.com>
2024-12-07 19:17:52 +0300

    login change

--- apps/static/assets/js/login/showerror.js
+++ apps/static/assets/js/login/showerror.js
@@ -1,5 +1,4 @@
  function showError() {
-            // Display the error message when Forgot Password link is clicked
             const errorMessage = document.getElementById("error-message");
             errorMessage.style.display = "block";  // Make the error message visible
         }
